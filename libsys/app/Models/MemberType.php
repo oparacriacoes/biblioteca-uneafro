ebedd7e45d8a6b141670c13d5e8e0f5d992968a2
Lucas Monteiro <lucasmmcota@gmail.com>
2023-05-29 15:23:37 -0300

    Criando models do sistema.

---
+++ libsys/app/Models/MemberType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MemberType extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'member_type';
+}
