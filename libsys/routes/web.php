66cfd69c7f61a03a30d7bc1e1329628d0576c6eb
Lucas Monteiro <lucasmmcota@gmail.com>
2023-05-29 15:21:20 -0300

    Configurando rotas iniciais do sistema.

--- libsys/routes/web.php
+++ libsys/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/book', [BookController::class, 'index'])->name('book.index');
+    Route::get('/loan', [LoanController::class, 'index'])->name('loan.index');
+    Route::get('/member', [MemberController::class, 'index'])->name('member.index');
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/user_edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/user_edit', [UserController::class, 'update'])->name('user.update');
+    Route::put('/user_password', [UserController::class, 'password'])->name('user.password');
 });
