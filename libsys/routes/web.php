8cd919f0061f757dc383447b3ec34c517b5651c5
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-09 11:09:47 -0300

    Atualizando rotas.

--- libsys/routes/web.php
+++ libsys/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
@@ -19,10 +22,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('book', BookController::class);
