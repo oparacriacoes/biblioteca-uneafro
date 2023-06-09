3dc60eb6ca01034b5cb3bf850d5c7fa87b978b2c
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-09 11:25:53 -0300

    Alterando montagem da view de login.

--- libsys/resources/views/auth/login.blade.php
+++ libsys/resources/views/auth/login.blade.php
@@ -11,34 +11,35 @@
             <div class="card card-login card-white">
                 <div class="card-header">
                     <img src="{{ asset('black') }}/img/card-primary.png" alt="">
-                    <h1 class="card-title">{{ __('Entrar') }}</h1>
+                    <h1 class="card-title">Entrar</h1>
                 </div>
                 <div class="card-body">
                     <p class="text-dark mb-2">Digite seu email e a senha para entrar.</p>
-                    <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <i class="tim-icons icon-email-85"></i>
-                            </div>
-                        </div>
-                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}">
-                        @include('alerts.feedback', ['field' => 'email'])
-                    </div>
-                    <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <i class="tim-icons icon-lock-circle"></i>
-                            </div>
-                        </div>
-                        <input type="password" placeholder="{{ __('Senha') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
-                        @include('alerts.feedback', ['field' => 'password'])
-                    </div>
+                    @include(
+                            'components.input',
+                            [
+                                'placeholder' => 'Email',
+                                'name' => 'email',
+                                'maxLength' => 50,
+                                'icon' => 'tim-icons icon-email-85'
+                            ]
+                        )
+
+                        @include(
+                            'components.input',
+                            [
+                                'type' => 'password',
+                                'placeholder' => 'Senha',
+                                'name' => 'password',
+                                'icon' => 'tim-icons icon-lock-circle'
+                            ]
+                        )
                 </div>
                 <div class="card-footer">
-                    <button type="submit" href="" class="btn btn-primary btn-lg btn-block mb-3">{{ __('Confirmar') }}</button>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block mb-3">Confirmar</button>
                     <div class="pull-right">
                         <h6>
-                            <a href="{{ route('password.request') }}" class="link footer-link">{{ __('Esqueceu a senha?') }}</a>
+                            <a href="{{ route('password.request') }}" class="link footer-link">Esqueceu a senha?</a>
                         </h6>
                     </div>
                 </div>
