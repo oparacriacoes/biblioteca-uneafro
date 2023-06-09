8cab9fecac5b2a900d509569d13d95b4184b7f9e
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-09 11:16:03 -0300

    Atualizando pagina inicial.

--- libsys/resources/views/home.blade.php
+++ libsys/resources/views/home.blade.php
@@ -5,10 +5,15 @@
         <div class="container">
             <div class="header-body text-center mb-7">
                 <div class="row justify-content-center">
-                    <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome!') }}</h1>
+                    <div class="col-lg-8 col-md-6">
+                        <h1 class="text-white">Seja bem-vindo(a) ao Libsys!</h1>
                         <p class="text-lead text-light">
-                            {{ __('Use Black Dashboard theme to create a great project.') }}
+                            O Libsys é um sistema de gerenciamento de bibliotecas simples e eficiente.
+                        </p>
+                        <p class="text-lead text-light">
+                            Faça o login no sistema para conseguir realizar empréstimos e devoluções. Além disso,
+                            também é possível cadastrar livros e membros da sua instituição de ensino para
+                            um melhor controle.
                         </p>
                     </div>
                 </div>
