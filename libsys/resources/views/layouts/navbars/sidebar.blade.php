35f895e7d558ce61a33c86882671665f5c476bd9
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-09 11:25:06 -0300

    Atualizando informacoes do menu lateral.

--- libsys/resources/views/layouts/navbars/sidebar.blade.php
+++ libsys/resources/views/layouts/navbars/sidebar.blade.php
@@ -1,34 +1,39 @@
+@php
+    $user = in_array($pageSlug, ['user_control', 'create_user', 'edit_user']) ? 'true' : 'false';
+    $loan = in_array($pageSlug, ['loan_control', 'make_loan']) ? 'true' : 'false';
+@endphp
+
 <div class="sidebar">
     <div class="sidebar-wrapper">
         <div class="logo">
-            <a href="#" class="simple-text logo-mini">{{ __('MP') }}</a>
-            <a href="#" class="simple-text logo-normal">{{ __('Menu Principal') }}</a>
+            <a href="#" class="simple-text logo-mini">MP</a>
+            <a href="#" class="simple-text logo-normal">Menu Principal</a>
         </div>
         <ul class="nav">
             <li @if ($pageSlug == 'dashboard') class="active " @endif>
                 <a href="{{ route('dashboard') }}">
                     <i class="tim-icons icon-chart-pie-36"></i>
-                    <p>{{ __('Dashboard') }}</p>
+                    <p>Dashboard</p>
                 </a>
             </li>
             <li>
-                <a data-toggle="collapse" href="#loan" aria-expanded="false">
+                <a data-toggle="collapse" href="#loan" aria-expanded="{{ $loan }}">
                     <i class="tim-icons icon-notes"></i>
-                    <span class="nav-link-text" >{{ __('Empréstimos') }}</span>
+                    <span class="nav-link-text">Empréstimos</span>
                     <b class="caret mt-1"></b>
                 </a>
-                <div class="collapse" id="loan">
+                <div class="{{ $loan == 'true' ? 'collapse show' : 'collapse' }}" id="loan">
                     <ul class="nav pl-4">
-                        <li @if ($pageSlug == 'loan') class="active" @endif>
+                        <li @if ($pageSlug == 'loan_control') class="active" @endif>
                             <a href="{{ route('loan.index') }}">
                                 <i class="tim-icons icon-single-02"></i>
-                                <p>{{ __('Controle de Empréstimos') }}</p>
+                                <p>Controle de Empréstimos</p>
                             </a>
                         </li>
-                        <li @if ($pageSlug == 'loan') class="active" @endif>
+                        <li @if ($pageSlug == 'make_loan') class="active" @endif>
                             <a href="{{ route('loan.index') }}">
                                 <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ __('Realizar Empréstimo') }}</p>
+                                <p>Realizar Empréstimo</p>
                             </a>
                         </li>
                     </ul>
@@ -37,42 +42,42 @@
             <li @if ($pageSlug == 'book') class="active" @endif>
                 <a href="{{ route('book.index') }}">
                     <i class="fas fa-book"></i>
-                    <p>{{ __('Livros') }}</p>
+                    <p>Livros</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'member') class="active" @endif>
                 <a href="{{ route('member.index') }}">
                     <i class="fas fa-users"></i>
-                    <p>{{ __('Membros') }}</p>
+                    <p>Membros</p>
                 </a>
             </li>
             <li>
-                <a data-toggle="collapse" href="#users" aria-expanded="false">
+                <a data-toggle="collapse" href="#users" aria-expanded="{{ $user }}">
                     <i class="fab fa-laravel" ></i>
-                    <span class="nav-link-text" >{{ __('Usuário') }}</span>
+                    <span class="nav-link-text">Usuário</span>
                     <b class="caret mt-1"></b>
                 </a>
-                <div class="collapse" id="users">
+                <div class="{{ $user == 'true' ? 'collapse show' : 'collapse' }}" id="users">
                     <ul class="nav pl-4">
-                        <li @if ($pageSlug == 'user_control') class="active" @endif>
-                            <a href="{{ route('user.index')  }}">
+                        <li @if ($pageSlug == 'user_control' || $pageSlug == 'create_user') class="active" @endif>
+                            <a href="{{ route('user.index') }}">
                                 <i class="tim-icons icon-single-02"></i>
-                                <p>{{ __('Controle de Usuários') }}</p>
+                                <p>Controle de Usuários</p>
                             </a>
                         </li>
                         <li @if ($pageSlug == 'edit_user') class="active" @endif>
-                            <a href="{{ route('user.edit', ['user' => auth()->user()]) }}">
+                            <a href="{{ route('user.edit', ['user' => auth()->user()->id]) }}">
                                 <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ __('Perfil') }}</p>
+                                <p>Perfil</p>
                             </a>
                         </li>
                     </ul>
                 </div>
             </li>
             <li>
-                <a href="{{ route('logout') }}" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fas fa-users"></i>
-                    <p>{{ __('Sair') }}</p>
+                    <p>Sair</p>
                 </a>
             </li>
         </ul>
