36a44adf0725dd1364ef5a9628b4a7281e21c5ea
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-12 20:54:26 -0300

    Adicionando componente select.

---
+++ libsys/resources/views/components/select.blade.php
@@ -0,0 +1,20 @@
+@php
+    $type = empty($oldValue) ? old($name) : old($name, $oldValue);
+@endphp
+
+<div class="input-group{{ $errors->has($name) ? ' has-danger' : '' }}">
+    <div class="input-group-prepend">
+        <div class="input-group-text">
+            <i class="{{ $icon }}"></i>
+        </div>
+    </div>
+    <select name="{{ $name }}" class="form-control{{ $errors->has($name) ? ' is-invalid' : '' }}">
+        <option value="">Selecione o tipo do membro</option>
+        @foreach ($slMemberType as $memberType)
+            <option value="{{ $memberType['id'] }}" {{ $memberType['id'] == $type ? 'selected' : '' }}>
+                {{ $memberType['type'] }}
+            </option>
+        @endforeach
+    </select>
+    @include('components.alerts.feedback', ['field' => $name])
+</div>
