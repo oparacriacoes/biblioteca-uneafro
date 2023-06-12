e2c3229086cb7a9f3e02f355c80abe97b954d196
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-12 20:52:08 -0300

    Criando request de validacao para insercao e alteracao de membros.

---
+++ libsys/app/Http/Requests/MemberRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Member;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class MemberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = unserialize($this->route('member'));
+        
+        return [
+            'full_name' => ['required', 'string', 'max:50'],
+            'id_member_type' => ['required', 'integer'],
+            'email' => ['nullable', 'string', 'email', 'max:50', 'unique:user,email',
+                Rule::unique((new Member())->getTable())->ignore($id)],
+            'phone' => ['nullable', 'integer', 'digits:11',
+                Rule::unique((new Member())->getTable())->ignore($id)],
+            'cpf' => ['nullable', 'integer', 'digits:11', 'unique:user,cpf',
+                Rule::unique((new Member())->getTable())->ignore($id)]
+        ];
+    }
+}
