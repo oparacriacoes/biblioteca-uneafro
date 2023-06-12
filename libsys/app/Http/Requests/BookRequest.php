8e2770e85cf0b5fbcda0c5ad2a8cd3051daa598c
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-12 17:17:25 -0300

    Criando validacoes para insercao e alteracao de livros.

---
+++ libsys/app/Http/Requests/BookRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Book;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string', 'max:50'],
+            'author' => ['required', 'string', 'max:50'],
+            'book_publisher' => ['required', 'string', 'max:30'],
+            'edition' => ['required', 'integer'],
+            'volume' => ['required', 'integer'],
+            'year' => ['required', 'integer'],
+            'number_of_copies' => ['required', 'integer'],
+            'number_of_reference_book' => ['required', 'integer'],
+            'ISBN' => ['required', 'integer', 'min_digits:10',
+                Rule::unique((new Book())->getTable())->ignore(auth()->id())]
+        ];
+    }
+}
