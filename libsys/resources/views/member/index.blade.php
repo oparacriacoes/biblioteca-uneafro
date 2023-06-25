b6fe527c0648eeb5f91749796934a4765e191783
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-25 17:31:07 -0300

    Atualizando pagina de membros para importar dados.

--- libsys/resources/views/member/index.blade.php
+++ libsys/resources/views/member/index.blade.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="col-6 text-right">
                         <a href="{{ route('member.create') }}" class="btn btn-sm btn-primary">Adicionar Membro</a>
-                        <a href="#" class="btn btn-sm btn-primary">Importar Membros</a>
+                        <a data-target="#import" data-toggle="modal" class="btn btn-sm btn-primary">Importar Membros</a>
                     </div>
                 </div>
             </div>
@@ -22,4 +22,22 @@
         </div>
     </div>
 </div>
+
+@include(
+    'components.modal_import',
+    [
+        'title' => 'Importar Membros',
+        'route' => 'member_import',
+        'message' => 'Para realizar a importação dos dados dos membros da sua instituição de ensino você deve criar um'
+            . ' arquivo .csv em que a primeira linha é o cabeçalho e as colunas são, respectivamente, os campos:'
+            . ' Nome Completo, Código do Tipo (1 = Professor(a) ou 2 = Aluno(a)), Email, Telefone, CPF. As demais'
+            . ' linhas devem ser os dados a serem inseridos. É importante que todos os dados estejam'
+            . ' preenchidos corretamente para evitar a ocorrência de erros.'
+    ]
+)
+
+<script src="{{ asset('black/js/components/import.js') }}"></script>
+<script src="{{ asset('black/js/validation/validator.js') }}"></script>
+<script src="{{ asset('black/js/member/import.js') }}"></script>
+
 @endsection
