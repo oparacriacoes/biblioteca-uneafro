5a68abb09a57bcc031d3af3651a2273a07f19c53
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-25 15:34:27 -0300

    Corrigindo erro ao recarregar valores no input.

--- libsys/resources/views/components/input.blade.php
+++ libsys/resources/views/components/input.blade.php
@@ -1,4 +1,7 @@
 @if (!empty($label)) <label>{{ $label }}</label> @endif
+@php
+    $type = empty($type) ? 'text' : $type;
+@endphp
 <div class="input-group{{ $errors->has($name) ? ' has-danger' : '' }}">
     <div class="input-group-prepend">
         <div class="input-group-text">
@@ -6,11 +9,13 @@
         </div>
     </div>
     <input
-        type="{{ empty($type) ? 'text' : $type }}"
+        type="{{ $type }}"
         name="{{ $name }}"
         class="form-control{{ $errors->has($name) ? ' is-invalid' : '' }}"
         placeholder="{{ empty($placeholder) ? $label : $placeholder }}"
-        @if (!empty($type) && $type != 'password') value="{{ empty($oldValue) ? old($name) : old($name, $oldValue) }}" @endif
+        @if (!empty($type) && $type != 'password')
+            value="{{ empty($oldValue) ? old($name) : old($name, $oldValue) }}"
+        @endif
         @if (!empty($maxLength)) maxlength="{{ $maxLength }}" @endif
     >
     @include('components.alerts.feedback', ['field' => $name])
