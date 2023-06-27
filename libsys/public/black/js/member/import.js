50c77effee880b918cca163310b32d69993ab3e3
Lucas Monteiro <lucasmmcota@gmail.com>
2023-06-26 21:24:37 -0300

    Corrigindo bugs na importacao de membros.

--- libsys/public/black/js/member/import.js
+++ libsys/public/black/js/member/import.js
@@ -11,20 +11,21 @@ document.getElementById("csv-file").addEventListener("change", function(event) {
         let csv = event.target.result;
         let lines = csv.split(/\r?\n/);
         let invalidRows = [];
+        let rowIndexes = [];
 
         // Verifica as linhas que contem valores invalidos
         for (let i = 1; i < lines.length - 1; i++) {
             let values = lines[i].split(";");
 
-            let validatedFullName = values[0] && validateFullName(values[0]);
+            let validatedName = values[0] && validateName(values[0]);
             let validatedIdMembeType = values[1] == 1 || values[1] == 2;
             let validatedEmail = isEmpty(values[2]) ? true : validateEmail(values[2]);
             let validatedPhone = isEmpty(values[3]) ? true : validatePhone(values[3]);
-            let validatedCPF = isEmpty(values[4]) ? true : validateCPF(values[4]);
+            let validatedCpf = isEmpty(values[4]) ? true : validateCpf(values[4]);
 
-            if (!validatedFullName || !validatedIdMembeType || !validatedEmail || !validatedPhone
-                    || !validatedCPF) {
+            if (!validatedName || !validatedIdMembeType || !validatedEmail || !validatedPhone || !validatedCpf) {
                 invalidRows.push(i + 1);
+                rowIndexes.push(i - 1);
             }
         }
 
@@ -35,13 +36,13 @@ document.getElementById("csv-file").addEventListener("change", function(event) {
             let message = "<p style='color: white;'>As seguintes linhas possuem valores inválidos e não serão"
                 + " incluídas: </p><ul>";
             for (const row of invalidRows) {
-                message += "<li>LInha: " + row + "</li>";
+                message += "<li>Linha: " + row + "</li>";
             }
             message += "</ul>";
             rowsDiv.innerHTML = message;
         }
         let invalidRowsInput = document.getElementById("invalid-rows");
-        invalidRowsInput.value = JSON.stringify(invalidRows);
+        invalidRowsInput.value = JSON.stringify(rowIndexes);
     };
     reader.readAsText(file);
 });
