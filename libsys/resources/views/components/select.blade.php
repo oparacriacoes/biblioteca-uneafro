e014f74bfaca3f3b6f976c99cce13af432141db6
Lucas Monteiro <lucasmmcota@gmail.com>
2023-07-07 15:17:41 -0300

    Organizar componente.

--- libsys/resources/views/components/select.blade.php
+++ libsys/resources/views/components/select.blade.php
@@ -2,18 +2,19 @@
     $type = empty($oldValue) ? old($name) : old($name, $oldValue);
 @endphp
 
+@if (!empty($label)) @include('components.label', ['label' => $label]) @endif
 <div class="input-group{{ $errors->has($name) ? ' has-danger' : '' }}">
     <div class="input-group-prepend">
         <div class="input-group-text">
             <i class="{{ $icon }}"></i>
         </div>
     </div>
-    <select name="{{ $name }}" class="form-control{{ $errors->has($name) ? ' is-invalid' : '' }}">
+    <select class="form-control{{ $errors->has($name) ? ' is-invalid' : '' }}" name="{{ $name }}">
         <option style="background-color: #344675;" value="">Selecione o tipo do membro</option>
         @foreach ($slMemberType as $memberType)
-            <option style="background-color: #344675;"value="{{ $memberType['id'] }}" {{ $memberType['id'] == $type ? 'selected' : '' }}>
-                {{ $memberType['type'] }}
-            </option>
+        <option style="background-color: #344675;" value="{{ $memberType['id'] }}" {{ $memberType['id'] == $type ? 'selected' : '' }}>
+            {{ $memberType['type'] }}
+        </option>
         @endforeach
     </select>
     @include('components.alerts.feedback', ['field' => $name])
