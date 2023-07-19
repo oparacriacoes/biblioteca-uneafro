29ec8790a5a87a10a9a8b93a8b3420b56e7389f0
Lucas Monteiro <lucasmmcota@gmail.com>
2023-07-19 19:23:58 -0300

    Criar view para tela de configurar condicoes do emprestimo.

---
+++ libsys/resources/views/loan_term/create.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app', ['page' => __('Configurações de Empréstimo'), 'pageSlug' => 'loan_config'])
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        @include('components.alerts.success', ['key' => 'success'])
+        <div class="card ">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-8">
+                        <h4 class="card-title">Configurações de Empréstimo</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <form class="form" method="post" action="{{ route('loan_term.store') }}">
+                    @csrf
+
+                    @include(
+                        'components.input',
+                        [
+                            'label' => 'Dias de empréstimo para aluno',
+                            'placeholder' => 'Dias de empréstimo para aluno',
+                            'name' => 'days_student',
+                            'maxLength' => 11,
+                            'oldValue' => !empty($loanTerm) ? $loanTerm['days_student'] : '',
+                            'icon' => 'fa-regular fa-calendar-days'
+                        ]
+                    )
+
+                    @include(
+                        'components.input',
+                        [
+                            'label' => 'Dias de empréstimo para professor',
+                            'placeholder' => 'Dias de empréstimo para professor',
+                            'name' => 'days_teacher',
+                            'maxLength' => 11,
+                            'oldValue' => !empty($loanTerm) ? $loanTerm['days_teacher'] : '',
+                            'icon' => 'fa-regular fa-calendar-days'
+                        ]
+                    )
+
+                    @include(
+                        'components.input',
+                        [
+                            'label' => 'Número máximo de renovações',
+                            'placeholder' => 'Número máximo de renovações',
+                            'name' => 'max_renovations',
+                            'maxLength' => 11,
+                            'oldValue' => !empty($loanTerm) ? $loanTerm['max_renovations'] : '',
+                            'icon' => 'fas fa-sync'
+                        ]
+                    )
+                    <div class="card-footer" style="text-align: center;">
+                        <button type="submit" class="btn btn-primary btn-round btn-lg">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
