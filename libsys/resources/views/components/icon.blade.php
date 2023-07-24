fe76a5c38d6c34fa770982f8f0c0136bf9f9cef2
Lucas Monteiro <lucasmmcota@gmail.com>
2023-07-24 15:57:23 -0300

    Adicionar target a icone que redireciona para nova guia.

--- libsys/resources/views/components/icon.blade.php
+++ libsys/resources/views/components/icon.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($title))
     <a title="{{ $title }}" data-target="{{ $target }}" data-toggle="{{ $dataToggle }}">
 @else
-    <a href="{{ $route }}">
+    <a href="{{ $route }}" @if (!empty($target)) target="{{ $target }}" @endif>
 @endif
     <i class="{{ $icon }}"></i>
 </a>
