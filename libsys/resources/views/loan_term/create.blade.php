8a68c23d2dc9d81b476a5290fef98497a791adb7
Lucas Monteiro <lucasmmcota@gmail.com>
2023-07-24 22:16:10 -0300

    Alterar disposicao dos campos na tela.

--- libsys/resources/views/loan_term/create.blade.php
+++ libsys/resources/views/loan_term/create.blade.php
@@ -15,42 +15,47 @@
             <div class="card-body">
                 <form class="form" method="post" action="{{ route('loan_term.store') }}">
                     @csrf
-
-                    @include(
-                        'components.input',
-                        [
-                            'label' => 'Dias de empréstimo para aluno',
-                            'placeholder' => 'Dias de empréstimo para aluno',
-                            'name' => 'days_student',
-                            'maxLength' => 11,
-                            'oldValue' => !empty($loanTerm) ? $loanTerm['days_student'] : '',
-                            'icon' => 'fa-regular fa-calendar-days'
-                        ]
-                    )
-
-                    @include(
-                        'components.input',
-                        [
-                            'label' => 'Dias de empréstimo para professor',
-                            'placeholder' => 'Dias de empréstimo para professor',
-                            'name' => 'days_teacher',
-                            'maxLength' => 11,
-                            'oldValue' => !empty($loanTerm) ? $loanTerm['days_teacher'] : '',
-                            'icon' => 'fa-regular fa-calendar-days'
-                        ]
-                    )
-
-                    @include(
-                        'components.input',
-                        [
-                            'label' => 'Número máximo de renovações',
-                            'placeholder' => 'Número máximo de renovações',
-                            'name' => 'max_renovations',
-                            'maxLength' => 11,
-                            'oldValue' => !empty($loanTerm) ? $loanTerm['max_renovations'] : '',
-                            'icon' => 'fas fa-sync'
-                        ]
-                    )
+                    <div class="row">
+                        <div class="col-4">
+                            @include(
+                                'components.input',
+                                [
+                                    'label' => 'Dias de empréstimo para aluno',
+                                    'placeholder' => 'Dias de empréstimo para aluno',
+                                    'name' => 'days_student',
+                                    'maxLength' => 11,
+                                    'oldValue' => !empty($loanTerm) ? $loanTerm['days_student'] : '',
+                                    'icon' => 'fa-regular fa-calendar-days'
+                                ]
+                            )
+                        </div>
+                        <div class="col-4">
+                            @include(
+                                'components.input',
+                                [
+                                    'label' => 'Dias de empréstimo para professor',
+                                    'placeholder' => 'Dias de empréstimo para professor',
+                                    'name' => 'days_teacher',
+                                    'maxLength' => 11,
+                                    'oldValue' => !empty($loanTerm) ? $loanTerm['days_teacher'] : '',
+                                    'icon' => 'fa-regular fa-calendar-days'
+                                ]
+                            )
+                        </div>
+                        <div class="col-4">
+                            @include(
+                                'components.input',
+                                [
+                                    'label' => 'Número máximo de renovações',
+                                    'placeholder' => 'Número máximo de renovações',
+                                    'name' => 'max_renovations',
+                                    'maxLength' => 11,
+                                    'oldValue' => !empty($loanTerm) ? $loanTerm['max_renovations'] : '',
+                                    'icon' => 'fas fa-sync'
+                                ]
+                            )
+                        </div>
+                    </div>
                     <div class="card-footer" style="text-align: center;">
                         <button type="submit" class="btn btn-primary btn-round btn-lg">Salvar</button>
                     </div>
