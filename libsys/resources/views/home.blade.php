97702357647522eeb0b8639f314a1510a94d8160
Lucas Monteiro <lucasmmcota@gmail.com>
2023-07-31 22:16:06 -0300

    Adicionar pageSlug a pagina home e so exibir mensagem caso login nao tenha sido realizado.

--- libsys/resources/views/home.blade.php
+++ libsys/resources/views/home.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.app', ['pageSlug' => 'home'])
 
-@section('content')
-    <div class="header py-7 py-lg-8">
-        <div class="container">
-            <div class="header-body text-center mb-7">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 col-md-6">
-                        <h1 class="text-white">Seja bem-vindo(a) ao Libsys!</h1>
-                        <p class="text-lead text-light">
-                            O Libsys é um sistema de gerenciamento de bibliotecas simples e eficiente.
-                        </p>
-                        <p class="text-lead text-light">
-                            Faça o login no sistema para conseguir realizar empréstimos e devoluções. Além disso,
-                            também é possível cadastrar livros e membros da sua instituição de ensino para
-                            um melhor controle.
-                        </p>
+@if (empty(auth()))
+    @section('content')
+        <div class="header py-7 py-lg-8">
+            <div class="container">
+                <div class="header-body text-center mb-7">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-md-6">
+                            <h1 class="text-white">Seja bem-vindo(a) ao Libsys!</h1>
+                            <p class="text-lead text-light">
+                                O Libsys é um sistema de gerenciamento de bibliotecas simples e eficiente.
+                            </p>
+                            <p class="text-lead text-light">
+                                Faça o login no sistema para conseguir realizar empréstimos e devoluções. Além disso,
+                                também é possível cadastrar livros e membros da sua instituição de ensino para
+                                um melhor controle.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
+@endif
