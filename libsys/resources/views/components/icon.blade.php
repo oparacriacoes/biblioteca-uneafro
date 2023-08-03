9a7c69b75cd7b926f6fe73d572d7462d79f08d79
Lucas Monteiro <lucasmmcota@gmail.com>
2023-08-03 18:38:29 -0300

    Adicionar classe a componente que gera icone.

--- libsys/resources/views/components/icon.blade.php
+++ libsys/resources/views/components/icon.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($dataTarget))
-    <a title="{{ $title }}" data-target="{{ $dataTarget }}" data-toggle="{{ $dataToggle }}">
+    <a class="text-secondary" title="{{ $title }}" data-target="{{ $dataTarget }}" data-toggle="{{ $dataToggle }}">
 @else
-    <a title="{{ $title }}" href="{{ $route }}" @if (!empty($target)) target="{{ $target }}" @endif>
+    <a class="text-secondary" title="{{ $title }}" href="{{ $route }}" @if (!empty($target)) target="{{ $target }}" @endif>
 @endif
     <i class="{{ $icon }}"></i>
 </a>
