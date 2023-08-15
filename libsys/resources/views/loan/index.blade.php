fa2d689fcc1d44c458e1f5c21e1073d7a2f7b2fe
Lucas Monteiro <lucasmmcota@gmail.com>
2023-08-15 15:13:57 -0300

    Adicionar barra de pesquisa em tela de emprestimo.

--- libsys/resources/views/loan/index.blade.php
+++ libsys/resources/views/loan/index.blade.php
@@ -11,6 +11,18 @@
                         <h4 class="card-title">Controle de Empréstimos</h4>
                     </div>
                 </div>
+                <div class="row mt-3">
+                    <div class="col-sm-12">
+                        @include(
+                            'components.input',
+                            [
+                                'name' => 'searchInput',
+                                'placeholder' => 'Pesquisar Empréstimo',
+                                'icon' => 'fa-solid fa-magnifying-glass'
+                            ]
+                        )
+                    </div>
+                </div>
             </div>
             <div class="card-body">
                 @include('components.table', ['arrayHeader' => $arrayHeader, 'arrayData' => $arrayData])
